4d78eda39ce6500a01271c8ce1e01ca328b28c7a
lirarahmawati20 <lirarahmawati94@gmail.com>
2023-10-17 20:37:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import {
+    kali,tambah,lingkaran
+} from "./mathOperation.js";
+
+console.log( "penjumlahan "+ tambah(9, 8));
+console.log( " perkalian " + kali(8, 7));
+console.log("pi "+ lingkaran(3.14159));
---
+++ mathOperation.js
@@ -0,0 +1,11 @@
+export function tambah  (a, b) {
+    return a + b ;
+};
+
+export function  kali(e, o) {
+    return e * o;
+};
+
+ export function lingkaran(r) {
+     return r ;
+ }
